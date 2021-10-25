11dc055a8be1b1bef99a8d3e6373d3d7e41fe4d9
Amodu Kehinde <amodukehindee@gmail.com>
2021-10-25 04:37:59 +0100

    fixed bugss

--- src/sections/values/ValuesSection.tsx
+++ src/sections/values/ValuesSection.tsx
@@ -8,7 +8,7 @@ import {
 } from "@chakra-ui/layout";
 // import Image from "next/image";
 
-import { TextStack } from "../../components/text-animations";
+// import { TextStack } from "../../components/text-animations";
 import { ContentGenerator } from "components/text-section";
 import content from "./content.json";
 
@@ -19,9 +19,8 @@ export const ValuesSection = () => {
         <Center p={10}>
         </Center>
         <Grid templateColumns="repeat(12, 1fr)" gap={2} alignItems="center">
-          <GridItem colSpan={[12, 6, 5]} mb={4}>
-            <TextStack word="MY VALUES" lines={2} />
-          </GridItem>
+          {/* <GridItem colSpan={[12, 6, 5]} mb={4}>
+          </GridItem> */}
           <GridItem colSpan={[12, 6, 6]}>
             <Stack spacing={2}>
               <ContentGenerator content={content} name="values-section" />
