b78b907922795fa404bca716fb6c357c0dc7a228
Amodu Kehinde <amodukehindee@gmail.com>
2022-05-31 17:12:54 +0100

    add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,12 +24,10 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#3182CE" />
 
           {/* add your own app-icon */}
-          {/* <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/apple-touch-icon.png"
-          />
-          <link rel="shortcut icon" href="/app-icon.png" /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+
           <link rel="manifest" href="/manifest.json" />
           <script
             async
